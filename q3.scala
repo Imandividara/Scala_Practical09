67fb3009bfa3da536133d56657d5d9c87fa71e8a
Imandi Muthugala <imandividaramuthugala@gmail.com>
2024-08-18 08:20:45 +0530

    done

--- q3.scala
+++ q3.scala
@@ -6,9 +6,9 @@ object StringFormatter {
   def formatNames(name: String)(formatFunc: String => String): String = formatFunc(name)
 
   def main(args: Array[String]): Unit = {
-    println(formatNames("Benny")(toUpper))         // Output: BENNY
-    println(formatNames("Niroshan")(name => name.substring(0, 2).toUpperCase + name.substring(2))) // Output: NIroshan
-    println(formatNames("Saman")(toLower))         // Output: saman
-    println(formatNames("Kumara")(name => name.init + name.last.toUpper))  // Output: KumarA
+    println(formatNames("Benny")(toUpper))  
+    println(formatNames("Niroshan")(name => name.substring(0, 2).toUpperCase + name.substring(2)))
+    println(formatNames("Saman")(toLower))         
+    println(formatNames("Kumara")(name => name.init + name.last.toUpper))
   }
 }
